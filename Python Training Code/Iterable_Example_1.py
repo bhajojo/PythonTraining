9c1446a32318c39b0a05c039c890475398ea6d81
bhajojo <joshibharat@live.com>
2018-06-19 07:39:18 +0530

    Day5_Completed

---
+++ Python Training Code/Iterable_Example_1.py
@@ -0,0 +1,17 @@
+# define a list
+my_list = [4, 7, 0, 3, 10,11,17]
+# get an iterator using iter()
+my_iter = iter(my_list)
+
+## iterate through it using next()
+#prints 4
+print(next(my_iter))
+#prints 7
+a=next(my_iter)
+print(next(my_iter))
+print(next(my_iter))
+print(next(my_iter))
+print(next(my_iter))
+print(next(my_iter))
+print(next(my_iter))
+
---
+++ Python Training Code/Iterable_Example_4.py
@@ -0,0 +1,15 @@
+l = ["bharat", "Mich", "Tom", "Harry"]
+for i in l:
+    print(i)
+
+print("\nDictionary Iteration")
+d = dict()
+d['xyz'] = 123
+d['abc'] = 345
+d['abc1'] = 346
+d['abc2'] = 347
+
+
+for i in d:
+    print("%d" % d[i])
+
